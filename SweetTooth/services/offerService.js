595a72eda36c559688cd66332d5dda62ff6809f4
Arnar Ólafsson <arnaro16@ru.is>
2019-09-30 16:34:11 +0000

    Service implementations

--- SweetTooth/services/offerService.js
+++ SweetTooth/services/offerService.js
@@ -3,8 +3,14 @@ const db = require('../data/data');
 const candyService = () => {
     const getAllOffers = (cb, errorCb) => {
         try {
-            const candies = db.candies;
-            cb(candies);
+            //const candies = db.candies;
+            //cb(candies);
+            var items = db.candies;
+            
+            
+            db.candies.forEach(element => {
+                
+            });
         } catch (err) {
             errorCb(err);
         }
